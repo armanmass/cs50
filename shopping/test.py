2dcc2eed0e622a2f9ee5a4c47c40b422dc00f49b
armanisme <armanmass2002@gmail.com>
2025-03-11 21:50:04 -0700

    shopping done nim done traffic added

---
+++ shopping/test.py
@@ -0,0 +1,34 @@
+import shopping
+
+shopping.load_data(f"shopping.csv")
+
+# wrap int
+Administrative0
+Informational2
+ProductRelated4
+OperatingSystems11
+Browser12
+Region13
+TrafficType14
+
+# wrap float
+Administrative_Duration1
+Informational_Duration3
+ProductRelated_Duration5
+BounceRates6
+ExitRates7
+PageValues8
+SpecialDay9
+
+#string to int
+Month10
+VisitorType15
+
+#bool to int
+Weekend16
+Revenue17
+
+int : 0 2 
+float: 1 3
+
+special : string to int
